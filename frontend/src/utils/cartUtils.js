aebd5d321168f1b71537809709c4185820de8792
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2023-08-21 16:26:34 +0530

    done with placing order & checkout process and user profile page

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -9,7 +9,7 @@ export const updateCart = (state) => {
   );
 
   // * Calculate shipping price (If order is over $100 then free, else $10 shipping)
-  state.sippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
 
   // * Calculate tax price (15% tax)
   state.taxPrice = addDecimals(Number(0.15 * state.itemsPrice).toFixed(2));
@@ -17,7 +17,7 @@ export const updateCart = (state) => {
   // * Calculate total price
   state.totalPrice = (
     Number(state.itemsPrice) +
-    Number(state.sippingPrice) +
+    Number(state.shippingPrice) +
     Number(state.taxPrice)
   ).toFixed(2);
 
